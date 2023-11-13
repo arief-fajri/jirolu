a5b9f6bf19583b90c2b86511b0df832a0ecad5c6
arief191 <arief@volantis.io>
2023-11-14 03:32:47 +0700

    add checkbox

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,3 +2,5 @@
 
 export { default as Text } from './Input/Text.svelte';
 export { default as Textarea } from './Input/Textarea.svelte';
+export { default as Checkbox } from './Input/Checkbox.svelte';
+export { default as Switch } from './Input/Switch.svelte';
