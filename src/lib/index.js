d214d4a3a52bfe0d427d87ff9590157174991147
arief191 <arief@volantis.io>
2023-11-14 03:10:41 +0700

    revoke v0.0.1

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,8 +1,3 @@
 // Reexport your entry components here
 
 export { default as Text } from './Input/Text.svelte';
-// export { default as Textarea } from './Input/Textarea.svelte';
-// export { default as Option } from './Input/Option.svelte';
-// export { default as OptionWithSearch } from './Input/OptionWithSearch.svelte';
-// export { default as Checkbox } from './Input/Checkbox.svelte';
-// export { default as Switch } from './Input/Switch.svelte';
