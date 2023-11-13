eeaa514c79254956886d131fb3d58eab05d5bc6b
arief191 <arief@volantis.io>
2023-11-14 03:50:09 +0700

    fix bugs

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,5 @@ export { default as Text } from './Input/Text.svelte';
 export { default as Textarea } from './Input/Textarea.svelte';
 export { default as Checkbox } from './Input/Checkbox.svelte';
 export { default as Switch } from './Input/Switch.svelte';
+export { default as Option } from './Input/Option.svelte';
+export { default as OptionWithSearch } from './Input/OptionWithSearch.svelte';
